c04f804fde4e8de00504de3b50b729abd2ddba10
Hermann-SW <hermann@stamm-wilbrandt.de>
2021-02-14 19:40:08 +0100

    First partially working max system clock determination tool per voltage.

--- multicore/multicore_hang/multicore.c
+++ multicore/multicore_hang/multicore.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/vreg.h"
 #include "pico/multicore.h"
@@ -15,8 +16,18 @@
 void tud_cdc_rx_wanted_cb(uint8_t itf, char wanted_char)
   { reset_usb_boot(0, 0); } // go to flash mode
 
+#define L 6
+unsigned char str[L+1];
+
+unsigned char *readLine() {
+  unsigned char u, *p;
+  for(p=str, u=getchar(); u!='\r' && p-str<L; u=getchar())  putchar(*p++=u);
+  *p = 0;  return str;
+}
+
 static int cnt=0;
 int cl;
+bool found = false;
 
 void core1_entry() {
     printf("%d\n", ++cnt);
@@ -24,13 +35,27 @@ void core1_entry() {
     uint32_t g = multicore_fifo_pop_blocking();
     multicore_fifo_push_blocking(123456);
 
-    for(cl=g; cl>130000; --cl)
+    for(cl=g; cl<700000; ++cl)
     {
+      if (found)
+        break;
+
       set_sys_clock_48mhz(); sleep_ms(2); printf("\r%d ",cl);
 
       if (set_sys_clock_khz(cl, false))
       {
         set_sys_clock_48mhz(); sleep_ms(2); printf("\n");
+ 
+        set_sys_clock_khz(cl, false);
+
+        for(int i=0; i<4; ++i)
+        {
+          gpio_put(2,1); while (gpio_get(3)!=1) {}
+          gpio_put(2,0); while (gpio_get(3)!=0) {}
+        }
+
+//        found = true;
+        set_sys_clock_48mhz(); sleep_ms(2); printf("found\n");
       }
     }
 
@@ -46,18 +71,26 @@ int main() {
     while (!tud_cdc_connected()) { sleep_ms(100);  }
     printf("tud_cdc_connected()\n");
 
+    printf("voltage number in range 6..15\n");
+    int vt = atoi(readLine());
+    while (VREG_VOLTAGE_MIN > vt ||  VREG_VOLTAGE_MAX < vt)
+    {
+      vt = atoi(readLine());
+    }
+    printf("\nfrequency\n");
+    int fr = atoi(readLine());
 
     gpio_init(2);  gpio_set_dir(2, GPIO_OUT);
-    gpio_init(3);  gpio_set_dir(3, GPIO_OUT);
+    gpio_init(3);  gpio_set_dir(3, GPIO_IN);
 
-    vreg_set_voltage(VREG_VOLTAGE_1_25);
+    vreg_set_voltage(vt); // VREG_VOLTAGE_1_25
     sleep_ms(1);
 
-    printf("Hello, multicore!\n");
+    printf("\nHello, multicore!\n");
 
     multicore_launch_core1(core1_entry);
 
-    multicore_fifo_push_blocking(408099);
+    multicore_fifo_push_blocking(fr);
     uint32_t g = multicore_fifo_pop_blocking();
 
   for(;;)
@@ -68,6 +101,9 @@ int main() {
       sleep_ms(5);
     }
 
+    if (found)
+      break;
+
     set_sys_clock_48mhz(); sleep_ms(2); printf("cl=%d\n",cl);
 
     sleep_ms(10);
@@ -76,9 +112,11 @@ int main() {
 
     multicore_launch_core1(core1_entry);
 
-    multicore_fifo_push_blocking(cl-1);
+    multicore_fifo_push_blocking(cl+1);
     g = multicore_fifo_pop_blocking();
   }
+
+    printf("Done.\n");
 
     for(;;){}
 }
